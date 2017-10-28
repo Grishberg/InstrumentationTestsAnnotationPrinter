e8c41dfaf332f01e1b9fcc3ccbbf3bbc6525d36f
Grigory Rylov <grishberg@gmail.com>
2017-10-29 01:46:45 +0300

    initial commit

---
+++ annotationtestprinter/src/main/java/com/github/grishberg/annotationprinter/AnnotationsTestPrinter.java
@@ -0,0 +1,50 @@
+package com.github.grishberg.annotationprinter;
+
+import android.os.Bundle;
+import android.support.test.internal.runner.listener.InstrumentationResultPrinter;
+import android.support.test.internal.runner.listener.InstrumentationRunListener;
+
+import org.junit.runner.Description;
+
+import java.lang.annotation.Annotation;
+import java.util.Collection;
+
+/**
+ * source https://github.com/medisafe/run-android-tests
+ */
+public class AnnotationsTestPrinter extends InstrumentationRunListener {
+
+    @Override
+    public void testStarted(Description description) throws Exception {
+        super.testStarted(description);
+
+        Collection<Annotation> annotations = description.getAnnotations();
+        if (annotations == null) {
+            return;
+        }
+
+        Bundle bundle = new Bundle();
+        StringBuilder annotationsBuilder = new StringBuilder();
+        boolean isNeedComm = false;
+
+        for (Annotation annotation : annotations) {
+            if (isNeedComm) {
+                annotationsBuilder.append(",");
+            }
+            annotationsBuilder.append(annotation.annotationType().getSimpleName());
+
+            // special case of AnnotationWithIndex
+            if (annotation instanceof Parameterized.AnnotationWithIndex) {
+                Parameterized.AnnotationWithIndex repeat = (Parameterized.AnnotationWithIndex) annotation;
+                annotationsBuilder.append(":");
+                annotationsBuilder.append(repeat.count());
+            }
+
+            isNeedComm = true;
+        }
+
+        bundle.putString("annotations", annotationsBuilder.toString());
+        getInstrumentation()
+                .sendStatus(InstrumentationResultPrinter.REPORT_VALUE_RESULT_START, bundle);
+    }
+}
